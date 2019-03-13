5f959761215d3e382d3af82b039c2fd9e067594a
Ioan Scurtu <ioan.scurtu120@gmail.com>
2019-03-13 14:46:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
+
+
+if (name === "Susan") {
+  console.log("The name is correct")
+} else {
+  console.log("Expecterd the name to be Susan.")
+}
+
+var name = "Susan"
